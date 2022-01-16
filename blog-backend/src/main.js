779e0cce7ad0e2dd8a6f3bd9f92c7791269b5627
yewl1110 <38202551+yewl1110@users.noreply.github.com>
2022-01-16 23:15:12 +0900

    build 처리

--- blog-backend/src/main.js
+++ blog-backend/src/main.js
@@ -7,6 +7,9 @@ import api from './api'
 import createFakeData from './createFakeData'
 import jwtMiddleware from './lib/jwtMiddleware'
 const cors = require('@koa/cors')
+import serve from 'koa-static'
+import path from 'path'
+import send from 'koa-send'
 
 const { PORT, MONGO_URI } = process.env
 
@@ -31,6 +34,14 @@ app.use(jwtMiddleware)
 router.use('/api', api.routes())
 
 app.use(router.routes()).use(router.allowedMethods())
+
+const buildDirectory = path.resolve(__dirname, '../../blog-frontend/build')
+app.use(serve(buildDirectory))
+app.use(async (ctx) => {
+	if (ctx.status === 404 && ctx.path.indexOf('/api') !== 0) {
+		await send(ctx, 'index.hmtl', { root: buildDirectory })
+	}
+})
 
 const port = PORT || 4000
 app.listen(port, () => {
